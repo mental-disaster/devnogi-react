e55ce8399cb9e4fe389e8147a51caab9c90be621
mental-disaster <dummy618234@gmail.com>
2025-06-23 22:59:23 +0900

    test: auction 테스트 코드 작성

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   // 상대 경로 매핑
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
---
+++ src/components/page/auction/search.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import AuctionSearch from "./search";
+import { ItemCategory } from "@/data/item-category";
+
+const createMockPath = (): ItemCategory[] => [
+  { id: "all", name: "전체" },
+  { id: "melee", name: "근거리 장비" },
+  { id: "one-handed", name: "한손 장비" },
+];
+
+describe("AuctionSearch", () => {
+  it("카테고리 배지 클릭 동작 테스트", () => {
+    const mockPath = createMockPath();
+    const mockOnCategorySelect = jest.fn();
+
+    render(
+      <AuctionSearch path={mockPath} onCategorySelect={mockOnCategorySelect} />,
+    );
+
+    const categoryBadge = screen.getByText("근거리 장비");
+    fireEvent.click(categoryBadge);
+
+    expect(mockOnCategorySelect).toHaveBeenCalledWith("melee");
+  });
+});
